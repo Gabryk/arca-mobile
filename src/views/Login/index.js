c3d89a7bb3759fc2737d7aa2ba8a81d88de2b0d6
Gabryk <gamadorsegura@gmail.com>
2019-09-03 10:28:30 -0600

    added login view

---
+++ src/views/Login/index.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+
+import {
+  View,
+} from 'react-native'
+import { Text } from 'react-native-elements'
+
+import theme from 'Theme/styles'
+
+
+export default class Login extends Component{
+  render(){
+    return (
+      <View style={theme.item}>
+        <View></View>
+        <View>
+          
+        </View>
+        <View></View>
+      </View>
+    )
+  }
+}
